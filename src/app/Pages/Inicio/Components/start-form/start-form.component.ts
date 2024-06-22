ad7322da45279b6b3c6331e0c093632b576eb568
Johmoreno <alexander.lond21@gmail.com>
2024-06-21 19:41:27 -0500

    arquitecture-design19

---
+++ src/app/Pages/Inicio/Components/start-form/start-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './start-form.component.html',
+  styleUrl: './start-form.component.css'
+})
+export class StartFormComponent {
+
+}
