75c9ea342c9b1b9fbc64d776190ff02671922fcf
Johmoreno <alexander.lond21@gmail.com>
2024-07-05 12:29:51 -0500

    arquitecturedesign21

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,23 +5,14 @@ export const routes: Routes = [
     {
         path: "inicio",
         title: "Inicio",
-        loadComponent: () => import("./Pages/Inicio/inicio.component").then((m) => m.InicioComponent)
+        loadComponent: () => import("./Pages/Inicio/inicio.component").then((m) => m.InicioComponent),
     },
     {
         path: "fundacion",
         title: "Fundacion",
         loadComponent: () => import("./Pages/Fundación/fundacion.component").then((m) => m.FundacionComponent)
     },
-    {
-        path: "contactenos",
-        title: "Contactenos",
-        loadComponent: () => import("./Pages/Contáctenos/contactenos.component").then((m) => m.ContactenosComponent)
-    },
-    {
-        path: "noticias",
-        title: "Noticias",
-        loadComponent: () => import("./Pages/Noticias/noticias.component").then((m) => m.NoticiasComponent)
-    },
+
     {
         path: "que-hacemos",
         title: "Que-Hacemos",
@@ -31,5 +22,10 @@ export const routes: Routes = [
         path: "servicios",
         title: "Servicios",
         loadComponent: () => import("./Pages/Servicios/servicios.component").then((m) => m.ServiciosComponent)
+    },
+    {
+        path:"oportunidades",
+        title:"Oportunidades",
+        loadComponent: () => import("./Pages/Oportunidades/oportunidades.component").then((m) => m.OportunidadesComponent)
     }
 ];
