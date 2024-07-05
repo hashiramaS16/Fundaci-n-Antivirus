99d6859de1858ffd5c5d18fde8d7a857d354ae13
Johmoreno <alexander.lond21@gmail.com>
2024-07-05 17:03:38 -0500

    correctionLeads

--- src/app/Pages/Inicio/inicio.component.ts
+++ src/app/Pages/Inicio/inicio.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { StartFormComponent } from './Components/start-paragraph/start-form.component';
+import { StartSlideComponent } from './Components/start-slide/start-slide.component';
+import { StartTextImageComponent } from './Components/start-text-image/start-text-image.component';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [StartFormComponent,StartSlideComponent,StartTextImageComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
