b66fa757371e98ce769fe5af410da9ed9d6e258d
Monica Taborda <monictaborda@hotmail.com>
2024-07-08 09:52:31 -0500

    feature/feature/form/finalización button-whatsapp y formulario

--- src/app/Pages/Oportunidades/Pages/Contáctenos/contactenos.component.ts
+++ src/app/Pages/Oportunidades/Pages/Contáctenos/contactenos.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ContentComponent } from './Components/content/content.component';
+import { BDComponent } from './Components/Formulario/bd.component';
+import { HeroComponent } from './Components/hero/hero.component';
 
 @Component({
   selector: 'app-contactenos',
   standalone: true,
-  imports: [],
+  imports: [ContentComponent, BDComponent, HeroComponent],
   templateUrl: './contactenos.component.html',
   styleUrl: './contactenos.component.css'
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './Pages/Components/nav-bar/nav-bar.component';
+import { ButtonWhatsappComponent } from './Pages/Components/button-whatsapp/button-whatsapp.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent],
+  imports: [RouterOutlet,NavBarComponent, ButtonWhatsappComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
