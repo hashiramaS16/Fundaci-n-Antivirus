1f79084ff6837fffae8f50fb130973a20047eeb7
klaw41 <claudreatr@gmail.com>
2024-07-12 12:36:28 -0500

    feature/feature/hero-contactenos/

--- src/app/Pages/Inicio/inicio.component.ts
+++ src/app/Pages/Inicio/inicio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { StartTextImageComponent } from './Components/start-text-image/start-text-image.component';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [StartTextImageComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
