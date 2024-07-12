0166e964e2836d6ec5379ca886df56e9bfd927d5
kamilo9809 <camilorivera09@outlook.com>
2024-07-12 02:56:55 -0500

    feature/content/finalizacion tarea

--- src/app/Pages/Oportunidades/Pages/Contáctenos/contactenos.component.ts
+++ src/app/Pages/Oportunidades/Pages/Contáctenos/contactenos.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from './Components/hero/hero.component';
+import { BDComponent } from './Components/Formulario/bd.component';
+import { ContentComponent } from './Components/content/content.component';
 
 @Component({
   selector: 'app-contactenos',
   standalone: true,
-  imports: [],
+  imports: [HeroComponent,BDComponent,ContentComponent],
   templateUrl: './contactenos.component.html',
   styleUrl: './contactenos.component.css'
 })
