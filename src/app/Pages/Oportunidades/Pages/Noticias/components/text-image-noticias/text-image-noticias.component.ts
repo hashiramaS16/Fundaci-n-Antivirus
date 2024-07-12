156df9ad09f7cd76ce295d3107cc03f529bb6055
kamilo9809 <camilorivera09@outlook.com>
2024-07-12 12:34:00 -0500

    integracion cambios claudia

---
+++ src/app/Pages/Oportunidades/Pages/Noticias/components/text-image-noticias/text-image-noticias.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-image-noticias',
+  standalone: true,
+  imports: [],
+  templateUrl: './text-image-noticias.component.html',
+  styleUrl: './text-image-noticias.component.css'
+})
+export class TextImageNoticiasComponent {
+
+}
--- src/app/Pages/Oportunidades/Pages/Noticias/noticias.component.ts
+++ src/app/Pages/Oportunidades/Pages/Noticias/noticias.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TextImageNoticiasComponent } from './components/text-image-noticias/text-image-noticias.component';
 
 @Component({
   selector: 'app-noticias',
   standalone: true,
-  imports: [],
+  imports: [TextImageNoticiasComponent],
   templateUrl: './noticias.component.html',
   styleUrl: './noticias.component.css'
 })
