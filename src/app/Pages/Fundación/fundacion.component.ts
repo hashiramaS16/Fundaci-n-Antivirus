2d2ae0c0ca2dad0a29bdef98572b29fa977d0d5d
Johmoreno <alexander.lond21@gmail.com>
2024-07-29 21:24:16 -0500

    updateleads

--- src/app/Pages/Fundación/fundacion.component.ts
+++ src/app/Pages/Fundación/fundacion.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { GallerySectionSlideComponent } from "./Components/gallery-section-slide/gallery-section-slide.component";
+import { HeroComponent } from "../Oportunidades/Pages/Cont\u00E1ctenos/Components/hero/hero.component";
+import { InfoCompanyComponent } from "./Components/info-company/info-company.component";
+import { SectionAboutInfoComponent } from "./Components/section-about-info/section-about-info.component";
+import { SectionVideoComponent } from "./Components/section-video/section-video.component";
 
 @Component({
   selector: 'app-fundacion',
   standalone: true,
-  imports: [],
+  imports: [GallerySectionSlideComponent, HeroComponent, InfoCompanyComponent, SectionAboutInfoComponent, SectionVideoComponent],
   templateUrl: './fundacion.component.html',
   styleUrl: './fundacion.component.css'
 })
