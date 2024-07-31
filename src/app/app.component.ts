c027f328c87ca9ae7126f43b98241aab264844ee
santiago acevedo <santiago_colorado82202@elpoli.edu.co>
2024-07-31 18:16:18 -0500

    creacion de los botones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './Pages/Components/nav-bar/nav-bar.component';
 import { ButtonWompiComponent } from './Pages/Components/button-wompi/button-wompi.component';
+import { SectionAboutInfoComponent } from './Pages/Fundación/Components/section-about-info/section-about-info.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent,ButtonWompiComponent],
+  imports: [RouterOutlet, NavBarComponent,ButtonWompiComponent,SectionAboutInfoComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'] // Corrected from styleUrl to styleUrls
 })
