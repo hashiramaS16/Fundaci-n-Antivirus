cbe7c7780f0f2e184ad3ff679ed5a6916ca84ff2
santiago acevedo <santiago_colorado82202@elpoli.edu.co>
2024-08-11 22:53:13 -0500

    Se hace la sección hero de acuerdo a Figma para la sección 'Contáctenos'

--- src/app/Pages/Contáctenos/contactenos.component.ts
+++ src/app/Pages/Contáctenos/contactenos.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from './Components/hero/hero.component';
 
 @Component({
   selector: 'app-contactenos',
   standalone: true,
-  imports: [],
+  imports: [ContactenosComponent],
   templateUrl: './contactenos.component.html',
   styleUrl: './contactenos.component.css'
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './Pages/Components/nav-bar/nav-bar.component';
 import { ButtonWompiComponent } from './Pages/Components/button-wompi/button-wompi.component';
 import { SectionAboutInfoComponent } from './Pages/Fundación/Components/section-about-info/section-about-info.component';
-import { HeroComponent } from './Pages/Fundación/Components/hero/hero.component';
-
-
+import { HeroComponent } from './Pages/Contáctenos/Components/hero/hero.component';
 
 @Component({
   selector: 'app-root',
